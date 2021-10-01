99304b6cb322240a6820978d1d0346ca72d4c21a
Sergey Vilgelm <sergey@vilgelm.com>
2021-10-01 10:51:10 -0700

    Use Go v1.17

--- reader.go
+++ reader.go
@@ -8,6 +8,6 @@ import (
 type Reader interface {
 	// Read reads the data and returns a raw data
 	Read(ctx context.Context) (interface{}, error)
-	// Returns a prefix to be used for all keys of the values provided by the reader
+	// Prefix returns a prefix to be used for all keys of the values provided by the reader
 	Prefix() string
 }
